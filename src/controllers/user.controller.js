c6125a221fb25f45463e87e32145aad45a4acfd2
Osmar Ortíz <osmar.ortiz@innappgt.com>
2023-05-25 21:43:02 -0600

    Prueba

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,6 +23,11 @@ exports.test = (req, res) => {
 exports.register_OnlyAdmin = async (req, res) => {
   try {
     const params = req.body
+    console.log(
+      '🚀 ~ file: user.controller.js:26 ~ exports.register_OnlyAdmin= ~ params:',
+      params
+    )
+
     const data = {
       name: params.name,
       username: params.username,
@@ -44,7 +49,10 @@ exports.register_OnlyAdmin = async (req, res) => {
       })
     }
 
-    if (params.role != 'ADMIN' || params.role != 'EMPLOYEE') {
+    console.log("role === 'ADMIN'", params.role === 'ADMIN')
+    console.log("role === 'EMPLOYEE'", params.role === 'EMPLOYEE')
+
+    if (params.role !== 'ADMIN' || params.role !== 'EMPLOYEE') {
       return res.status(401).send({ message: 'El rol no es válido' })
     }
 
